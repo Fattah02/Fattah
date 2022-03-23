7a8157119c5aaf315a3a9213f99be91ecfdeb870
Mohamed Fattah <mohamad.fattah02@gmail.com>
2022-03-23 10:49:14 +0100

    test entree github

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 #from utilities import *
 
 
+#test fattah
+
 #on genere un dataset X y, 100 lignes et 2 var
 X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
 y = y.reshape((y.shape[0]), 1)
